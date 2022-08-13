24608009eea70871d1b152fa567ee4072baf9fdc
hanif <hanif@pop-os.localdomain>
2022-08-13 10:45:05 +0700

    njij

---
+++ main.js
@@ -0,0 +1,30 @@
+// //1
+// let myArray=["hello  world"]
+// for (item of myArray) {
+//     console.log(item);
+// }
+// //2
+// let a={senin:55400,
+//     selasa:61600,
+//     rabu:49300,
+//     kamis:53200,
+//     jumat:54500,
+//     sabtu:51500,
+//     minggu:70000}
+    
+// for (let x in  a) {
+//     console.log(a[x]);
+// }
+
+// //3
+// let laporan=[{
+//     jam:'12.10',
+//     masuk:"tidak dikenali"
+// },{
+//     jam:'12.14',
+//     masuk:"tidak dikenali"
+// }]
+// for (item of laporan) {
+//     console.table(item);
+// }
+// //4
